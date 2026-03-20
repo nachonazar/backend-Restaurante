e70334b1249728d2a537f6966d3e9200cb62d7bb
Facu <facundomartinezr03@gmail.com>
2026-03-20 16:38:28 -0300

    fix

---
+++ src/modules/users/user.model.js
@@ -0,0 +1,41 @@
+import mongoose, { Schema } from "mongoose";
+
+const userSchema = new Schema({
+  userName: {
+    type: String,
+    required: true,
+    trim: true,
+    minlength: 3,
+    maxlength: 100,
+  },
+  email: {
+    type: String,
+    required: true,
+    minlength: 5,
+    maxlength: 100,
+    unique: true,
+    lowercase: true,
+    trim: true,
+    match: [
+      /^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$/,
+    ],
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  role: {
+    type: String,
+    enum: ["User", "Admin"],
+    default: "User",
+  },
+  status: {
+    type: String,
+    enum: ["Activo", "Suspendido"],
+    default: "Activo",
+  },
+}, { timestamps: true });
+
+const User = mongoose.model("User", userSchema);
+
+export default User;
