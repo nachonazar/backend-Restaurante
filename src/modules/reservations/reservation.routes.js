9c9cf6729729b1a3773b067e45070e15d2b32a97
Ignacio <ignacionazar9@gmail.com>
2026-03-24 19:42:41 -0300

    actualice las rutas con verifyToken y validaciones

--- src/modules/reservations/reservation.routes.js
+++ src/modules/reservations/reservation.routes.js
@@ -1,5 +1,35 @@
 import { Router } from "express";
+import { verifyToken } from "../../middlewares/auth.middleware.js";
+import { requireAdmin } from "../../middlewares/role.middleware.js";
+import { handleValidation } from "../../shared/handleValidation.js";
+import {
+  validateCreateReservation,
+  validateUpdateReservation,
+} from "./reservation.validator.js";
+import {
+  leerReservas,
+  crearReserva,
+  leerReservaPorId,
+  editarReservaPorId,
+  borrarReservaPorId,
+} from "./reservation.controller.js";
 
 const router = Router();
+
+router
+  .route("/")
+  .get(verifyToken, leerReservas)
+  .post(verifyToken, validateCreateReservation, handleValidation, crearReserva);
+
+router
+  .route("/:id")
+  .get(verifyToken, leerReservaPorId)
+  .delete(verifyToken, borrarReservaPorId)
+  .put(
+    verifyToken,
+    validateUpdateReservation,
+    handleValidation,
+    editarReservaPorId,
+  );
 
 export default router;
