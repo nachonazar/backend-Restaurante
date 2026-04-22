96be04e5a008e05be07b3ddc1a173ae57e58ad2d
Facu <facundomartinezr03@gmail.com>
2026-04-22 15:57:11 -0300

    cambie el conectdb a app

--- app.js
+++ app.js
@@ -1,28 +1,22 @@
 import express from "express";
 import cors from "cors";
 import morgan from "morgan";
-
-// Rutas
+import connectDB from "./src/config/db.js";
 import authRoutes from "./src/modules/auth/auth.routes.js";
 import userRoutes from "./src/modules/users/user.routes.js";
 import reservationRoutes from "./src/modules/reservations/reservation.routes.js";
-
-// Middleware de errores
 import { errorMiddleware } from "./src/middlewares/error.middleware.js";
 
 const app = express();
 
+connectDB(); 
+
 const origenesPermitidos = [
-  "http://localhost:5173", //localmente
-  "https://restaurant-front-topaz-iota.vercel.app", //frontend en Vercel
+  "http://localhost:5173",
+  "https://restaurant-front-topaz-iota.vercel.app",
 ];
 
-app.use(
-  cors({
-    origin: origenesPermitidos,
-    credentials: true,
-  }),
-);
+app.use(cors({ origin: origenesPermitidos, credentials: true }));
 app.use(express.json());
 app.use(morgan("dev"));
 
--- index.js
+++ index.js
@@ -1,10 +1,7 @@
 import "dotenv/config";
 import app from "./app.js";
-import connectDB from "./src/config/db.js";
 
 const PORT = process.env.PORT || 3001;
-
-connectDB();
 
 if (process.env.NODE_ENV !== "production") {
   app.listen(PORT, () => {
